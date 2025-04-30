e678b7e01751b2d3c10dd9f59e4d4f62a1a7ab2b
Sadwhis <tri22005.mb@gmail.com>
2025-04-30 20:22:55 -0300

    Sadwhis
    Say my name

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    string _nome ="guys escrevam qualquer coisa aqui pra ver se o coiso ta funcionando";
+
+
+
+    void Start()
+    {
+        
+
+    }
+    
+  
+    void Update()
+    {
+        
+    }
+}
