86d0b8de7f142a3502964590e4dc8b7ba1fa0291
RafaAb-078 <rafaelakazumi.05@gmail.com>
2025-04-30 20:25:37 -0300

    ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     string _nome ="guys escrevam qualquer coisa aqui pra ver se o coiso ta funcionando";
+    string _nome1 = "ta tudo certo aqui no meu";
 
 
 
     void Start()
     {
-        
+       
 
     }
     
