6b357042d6e6e1c49627f013b471c8fb35ac3e45
Rex-Soul <yuriyam407@gmail.com>
2025-04-30 20:31:00 -0300

    Rafaela com tpm

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     string _nome ="guys escrevam qualquer coisa aqui pra ver se o coiso ta funcionando";
     string _nome1 = "ta tudo certo aqui no meu";
+    string _NOME = "Rafaela para de ficar de tpm o tempo todo, não vai ganha chocolate";
 
 
 
