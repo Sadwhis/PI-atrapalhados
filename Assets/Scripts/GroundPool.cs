8a023e96d598bfa7a509d9722ed58afbddedcdc7
Rex-Soul <yuriyam407@gmail.com>
2025-06-03 21:35:11 -0300

    Mini-Jogo

---
+++ Assets/Scripts/GroundPool.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GroundPool : ObjectPool
+{
+    public static GroundPool _groundPool;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _groundPool = this;
+    }
+}
