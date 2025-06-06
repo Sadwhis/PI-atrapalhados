67982ddfc260efd9c9e64a0f4f50c058be21afb9
Rex-Soul <yuriyam407@gmail.com>
2025-06-05 21:30:28 -0300

    sapo indio

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -7,12 +7,16 @@ public class GameControl : MonoBehaviour
     [SerializeField] Transform _groundBase;
     [SerializeField] float _groundH;
     [SerializeField] float _distance;
+    [SerializeField] bool _checkGroundCount;
+    public int _groundNumber;
     void Start()
     {
         _groundH = _groundBase.position.y;
         for (int i = 0; i < 10; i++)
         {
            Invoke("GroundStart", 0.25f);
+           
+            
         }
         
     }
@@ -25,8 +29,20 @@ public class GameControl : MonoBehaviour
         {
             bullet.transform.position = new Vector2(bullet.transform.position.x, _groundH + _distance); 
             _groundH = bullet.transform.position.y;
+            
             //bullet.transform.rotation = turret.transform.rotation;
             bullet.SetActive(true);
+        }
+    }
+    void GroundTime()
+    {
+        for (int i = 0;i<_groundNumber;i++)
+        {
+            GroundStart();
+            if (i < _groundNumber - 2)
+            {
+                _checkGroundCount = true;
+            }
         }
     }
 }
