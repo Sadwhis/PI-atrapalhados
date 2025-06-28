b728040b8aeae93596da7987fd1462e9752d17a7
Rex-Soul <yuriyam407@gmail.com>
2025-06-27 21:35:29 -0300

    sapo-cururu

--- Assets/Scripts/CenaMenuController.cs
+++ Assets/Scripts/CenaMenuController.cs
@@ -7,9 +7,21 @@ using JetBrains.Annotations;
 public class CenaMenuController : MonoBehaviour
 {
     [SerializeField] Transform[] _painelStart;
+    [SerializeField] Transform[] _painelconfig;
     void Start()
     {
-        _painelStart[0].DOScale(1, 3);
+        for (int i = 0; i < _painelStart.Length; i++)
+        {
+          _painelStart[i].localScale = Vector3.zero;
+            
+        }
+        for (int i = 0; i < _painelconfig.Length; i++)
+        {
+            _painelconfig[i].localScale = Vector3.zero;
+
+        }
+        PainelStartCheck(true);
+        PainelConfigOff();
     }
 
     // Update is called once per frame
@@ -26,11 +38,49 @@ public class CenaMenuController : MonoBehaviour
          _painelStart[i].DOScale(1, .25f);
         }
     }
+    IEnumerator TimeConfig()
+    {
+        for (int i = 0; i < _painelconfig.Length; i++)
+        {
+            _painelconfig[i].DOScale(1.5f, .25f);
+            yield return new WaitForSeconds(0.25f);
+            _painelconfig[i].DOScale(1, .25f);
+        }
+    }
     public void PainelStartOff()
     {
         for (int i = 0; i < _painelStart.Length; i++)
         {
             _painelStart[i].DOScale(0, .25f);
+        }
+    }
+    public void PainelStartCheck(bool checkON)
+    {
+        if(checkON == true)
+        {
+            StartCoroutine(TimeStart());
+        }
+        else
+        {
+            PainelStartOff();
+        }
+    }
+    public void PainelConfigCheck(bool checkON)
+    {
+        if (checkON == true)
+        {
+            StartCoroutine(TimeConfig());
+        }
+        else
+        {
+            PainelConfigOff();
+        }
+    }
+    public void PainelConfigOff()
+    {
+        for (int i = 0; i < _painelconfig.Length; i++)
+        {
+            _painelconfig[i].DOScale(0, .25f);
         }
     }
 }
