496021a3653df4d3f7782732d11e2b96c7af5749
Leandro Pinheiro <Leandrob2612@Gmail.com>
2026-03-06 19:49:17 -0300

    Dialogo Inicial

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,26 +1,44 @@
-using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
+
 
 public class NPC : MonoBehaviour
 {
     [Header("Configurações")]
-    public GameObject _TextoFrog;
-    public GameObject _TextoFrog2;
     public float tempoAnimacao = 0.5f;
+    public float tempoPausa = 3f;
+    public List<GameObject> _textos = new List<GameObject>();
+
+    private int indiceAtual = 0;
+    private bool poClicar = true;
+
+    public GameObject buttonClicar;
+    public GameObject backGroundUI;
 
     private void Start()
     {
-       
+        foreach (var texto in _textos)
+        {
+            texto.transform.localScale = Vector3.zero;
+            texto.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            indiceAtual = 0;
+            poClicar = true;
+            buttonClicar.SetActive(true);
+            backGroundUI.SetActive(true);
+            MostrarTextoAtual();
 
-            _TextoFrog.transform.DOKill();
-            _TextoFrog.SetActive(true);
-            _TextoFrog.transform.DOScale(1, tempoAnimacao).SetEase(Ease.OutBack);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
@@ -28,11 +46,78 @@ public class NPC : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _TextoFrog.transform.DOKill();
-            _TextoFrog.transform.DOScale(0, tempoAnimacao).SetEase(Ease.InBack).OnComplete(() =>
-            {
-                _TextoFrog.SetActive(false);
-            });
+            EsconderTextoAtual();
+            
+            buttonClicar.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            backGroundUI.transform.DOKill();
+            backGroundUI.transform.DOScale(0, tempoAnimacao).SetEase(Ease.InBack);
         }
+    }
+
+    public void ProximoDialogo()
+    {
+        
+        if (!poClicar) return;
+
+        poClicar = false; 
+        EsconderTextoAtual();
+
+       
+        float tempoTotalDeEspera = tempoAnimacao + tempoPausa;
+
+        
+        DOVirtual.DelayedCall(tempoTotalDeEspera, () =>
+        {
+            indiceAtual++;
+
+            if (indiceAtual < _textos.Count)
+            {
+                MostrarTextoAtual();
+                poClicar = true; 
+            }
+            else
+            {
+                Debug.Log("Fim do didi!");
+
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                backGroundUI.SetActive(false);
+                buttonClicar.SetActive(false);
+
+                backGroundUI.transform.DOKill();
+                backGroundUI.transform.DOScale(0, tempoAnimacao).SetEase(Ease.InBack);
+
+            }
+        });
+    }
+
+    private void MostrarTextoAtual()
+    {
+        if (_textos.Count == 0 || indiceAtual >= _textos.Count) return;
+
+        GameObject texto = _textos[indiceAtual];
+        backGroundUI.SetActive (true);
+        texto.SetActive(true);
+        backGroundUI.transform.DOKill();
+        texto.transform.DOKill();
+        //backGroundUI.transform.localScale = Vector3.zero;
+        texto.transform.localScale = Vector3.zero;
+        backGroundUI.transform.DOScale(1, tempoAnimacao).SetEase(Ease.OutBack);
+        texto.transform.DOScale(1, tempoAnimacao).SetEase(Ease.OutBack);
+    }
+
+    private void EsconderTextoAtual()
+    {
+        if (_textos.Count == 0 || indiceAtual >= _textos.Count) return;
+
+        GameObject texto = _textos[indiceAtual];
+        texto.transform.DOKill();
+        texto.transform.DOScale(0, tempoAnimacao).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            texto.SetActive(false);
+        });
+       
     }
 }
