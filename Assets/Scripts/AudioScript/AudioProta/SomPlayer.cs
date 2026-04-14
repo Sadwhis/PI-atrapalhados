226d0275aa735c876c105ec754e891b55da938ed
Rex-Soul <yuriyam407@gmail.com>
2026-04-14 19:38:46 -0300

    grama
    grama

---
+++ Assets/Scripts/AudioScript/AudioProta/SomPlayer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "SomProta", menuName = "SomProta")]
+
+public class SomProta : ScriptableObject
+{
+    public AudioClip andar;
+    public AudioClip hitPlayer;
+    public AudioClip puloPlayer;
+    public AudioClip punchPlayer;
+}
