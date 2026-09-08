877826d9d6b2aab3c59ddd1c455e729772451f2c
Rex-Soul <yuriyam407@gmail.com>
2026-09-08 19:40:09 -0300

    desculpa
    ajeitei os erros

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPause : MonoBehaviour
+{
+    public GameObject painel_do_menu;
+    public string nomeDaFase;
+    void Start()
+    {
+        painel_do_menu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseJoga();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CarregarNovaFase();
+        }
+    }
+    private void PauseJoga()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            painel_do_menu.SetActive(true);
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            painel_do_menu.SetActive(false);
+        }
+    }
+    public void Conitinuar()
+    {
+        PauseJoga();
+    }
+    private void CarregarNovaFase()
+    {
+        SceneManager.LoadScene(nomeDaFase);
+    }
+    public void config()
+    {
+    Debug.Log("Abrindo configurações");
+    }
+}
