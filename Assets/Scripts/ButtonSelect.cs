4ecb8e9cf54c11cacc10b80257d9275802c2076d
Rex-Soul <yuriyam407@gmail.com>
2026-09-17 19:25:55 -0300

    hud pelo controle
    só falta adicionar o pause

---
+++ Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour
+{
+    public Button primaryButton;
+    void Start()
+    {
+        primaryButton.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
